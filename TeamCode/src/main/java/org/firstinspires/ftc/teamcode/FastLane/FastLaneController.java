8da8a46a43502f44c4b17aa99087b46377720bac
0verkil <112268976+0verkil@users.noreply.github.com>
2024-08-17 18:32:51 -0700

    corrected comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FastLane/FastLaneController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FastLane/FastLaneController.java
@@ -8,8 +8,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import kotlin.NotImplementedError;
 
-// TODO: collision detection
-
 /**
  * Primary controller for the FastLane Path Following system.
  */
